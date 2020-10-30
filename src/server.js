671f40f33b5eb5572b50ebf41213cc884bbe3a12
zeze.li <lizeze.boy@gmail.com>
2020-10-31 00:08:37 +0800

    修改页面

--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ const port = 96;
 const path = require('path');
 const {readFile} = require('./tool');
 app.use(express.static('public'))
-
+app.use('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+  //res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+    if (req.method == 'OPTIONS') {
+      res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+    }
+    else {
+      next();
+    }
+  });
 app.get('/v1/api/xzhf', async (req, res) => {
     const {level, fid} = req.query;
     let filePath = '';
