e68efcd17d4ee7467f1daaa653868cfeda9887de
zeze.li <lizeze.boy@gmail.com>
2020-11-30 16:48:17 +0800

    增加npm scripts

--- src/postCode.js
+++ src/postCode.js
@@ -1,28 +1,6 @@
 const path = require('path');
 const {readFile, postCodeSqlObj} = require('./tool');
 const fs = require('fs')
-let fileDisplay = (filePath, level) => {
-    fs.readdir(filePath, async function (err, files) {
-        if (err) {
-        } else {
-            let sqlText = '';
-            for (let i = 0; i < files.length; i++) {
-                var filedir = path.join(filePath, files[i]);
-                let fileContent = await readFile(filedir);
-                sqlText += createSqlText(fileContent);
-            }
-            fs.writeFileSync(`./dist/${level}.sql`, sqlText, {encoding: 'utf8'});
-        }
-    });
-};
-
-let createSqlText = (array) => {
-    let sqlText = '';
-    array.forEach((item) => {
-        sqlText += `insert into  ${postCodeSqlObj.tableName}  values ('${item.id}', '${item.province}','${item.city}','${item.area}','${item.post_code}','${item.area_code}');\n`;
-    });
-    return sqlText;
-};
 
 let filePath = path.normalize(path.join(__dirname, "../data", "5", "/"));
 let main = async () => {
@@ -31,4 +9,12 @@ let main = async () => {
     let sqlText = createSqlText(data)
     fs.writeFileSync(`./dist/5.sql`, sqlText, {encoding: 'utf8'});
 }
+let createSqlText = (array) => {
+    let sqlText = '';
+    array.forEach((item) => {
+        sqlText += `insert into  ${postCodeSqlObj.tableName}  values ('${item.id}', '${item.province}','${item.city}','${item.area}','${item.post_code}','${item.area_code}');\n`;
+    });
+    return sqlText;
+};
+
 main()
